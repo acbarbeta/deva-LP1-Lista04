aafbd5508b00508ed14dd51689879ace0710baf3
Ana Clara <aclara.bvicente@gmail.com>
2023-12-14 23:10:21 -0300

    Exercício 03

--- src/Exercicio03.java
+++ src/Exercicio03.java
@@ -1,2 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Exercicio03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quantos elementos o array terá? ");
+        int numeroDeElementos = input.nextInt();
+
+        int[] nums = new int[numeroDeElementos];
+
+        for (int i = 0; i < numeroDeElementos; i++) {
+            System.out.printf("Digite o %dº elemento: %n", (i+1));
+            nums[i] = input.nextInt();
+        }
+
+        calculaSomaAcumulada(nums, numeroDeElementos);
+    }
+
+    public static void calculaSomaAcumulada(int[] nums, int numeroDeElementos) {
+
+        int somaAcumulada = 0;
+        int[] arrayDaSoma = new int[(numeroDeElementos)];
+
+        for (int i = 0; i < numeroDeElementos ; i++) {
+            somaAcumulada += nums[i];
+            arrayDaSoma[i] = somaAcumulada;
+        }
+
+
+        System.out.println(Arrays.toString(arrayDaSoma));
+    }
 }
