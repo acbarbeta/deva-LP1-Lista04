0ddd4b409aaa98ff24885dba75c074c964e7bd2f
Ana Clara <aclara.bvicente@gmail.com>
2023-12-15 21:56:19 -0300

    Exercício 05 - Final

--- src/Exercicio05.java
+++ src/Exercicio05.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Exercicio05 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Quantos elementos seu array terá?");
+        int numeroDeElementos = input.nextInt();
+
+        int[] numeros = new int[numeroDeElementos];
+
+        for (int i = 0; i < numeroDeElementos ; i++) {
+            System.out.printf("Digite o %dº elemento: %n", (i+1));
+            numeros[i] = input.nextInt();
+        }
+
+        int indiceDoPivot = encontraPivot(numeros);
+        System.out.printf("Índice do Pivot: %d", indiceDoPivot);
+    }
+
+    public static int encontraPivot(int[] numeros) {
+        int somaTotal = 0;
+        int somaLadoEsquerdo = 0;
+
+        for (int numero : numeros) {
+            somaTotal += numero;
+        }
+
+        for (int i = 0; i < numeros.length ; i++) {
+            somaTotal -= numeros[i];
+
+            if (somaTotal == somaLadoEsquerdo) {
+                return i;
+            }
+
+            somaLadoEsquerdo += numeros[i];
+        }
+
+        return -1;
+    }
 }
